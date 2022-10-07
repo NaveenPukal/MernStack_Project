e2d65ad8d11d9d9a09e4c681dd2516487be518ed
naveen <pukalnaveen98@gmail.com>
2022-10-07 13:04:57 +0530

    added single product page

--- back_end/index.js
+++ back_end/index.js
@@ -12,8 +12,16 @@ app.use(cors());
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
--- front_end/src/screens/HomeScreen.js
+++ front_end/src/screens/HomeScreen.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Axios from "axios";
 import Product from "../components/Product";
+import axios from "axios";
+import logger from "use-reducer-logger";
+import { Helmet } from "react-helmet-async";
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "FETCH_REQUEST":
@@ -16,25 +20,45 @@ const reducer = (state, action) => {
   }
 };
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
 
   useEffect(() => {
-    Axios.get("http://localhost:5000/api/products").then((response) => {
-      setProducts(response.data);
-    });
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("http://localhost:5001/api/products");
+
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+    };
+    fetchData();
   }, []);
 
   return (
     <div>
-      {" "}
+      <Helmet>
+        <title>Trendy</title>
+      </Helmet>{" "}
       <h1>Feature Products</h1>
       <div className="products">
         <Row>
-          {products.map((product) => (
-            <Col key={product.slug} sm={6} md={4} lg={3} className="mb=3">
-              <Product product={product}></Product>
-            </Col>
-          ))}
+          {loading ? (
+            <div>Loading..</div>
+          ) : error ? (
+            <div>{error}</div>
+          ) : (
+            products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb=3">
+                <Product product={product}></Product>
+              </Col>
+            ))
+          )}
         </Row>
       </div>
     </div>
